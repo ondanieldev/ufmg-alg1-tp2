0205694258c56c1bb27b8b4e29fe57e8363c6a0f
ondanieldev <me@ondaniel.com.br>
2022-06-19 00:22:40 -0300

    feat: get max weight using a reverse-like dijkstra

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,9 +2,13 @@
 #include <list>
 #include <vector>
 #include <stdexcept>
+#include <list>
+#include <algorithm>
 
 #include "Graph.h"
 #include "Edge.h"
+
+#define INFINITY 100001
 
 Graph::Graph(int nodes)
 {
@@ -31,4 +35,46 @@ void Graph::print(int src)
     std::cout << edge.dst << " (" << edge.weight << ")\t";
   }
   std::cout << std::endl;
+}
+
+int Graph::maxWeight(int src, int dst)
+{
+  int pi[this->nodes];
+  std::list<int> priorityQueue;
+
+  for (int v = 0; v < this->nodes; ++v)
+  {
+    pi[v] = INFINITY * (-1);
+    priorityQueue.push_back(v);
+  }
+  pi[src] = INFINITY;
+
+  while (!priorityQueue.empty())
+  {
+    int u = priorityQueue.front();
+    for (auto &v : priorityQueue)
+    {
+      if (pi[v] > pi[u])
+      {
+        u = v;
+      }
+    }
+    priorityQueue.remove(u);
+
+    if (pi[u] == (INFINITY * (-1)))
+    {
+      break;
+    }
+
+    for (auto &edge : this->adjacencyList[u])
+    {
+      int betterWeight = std::max(pi[edge.dst], std::min(pi[u], edge.weight));
+      if (betterWeight > pi[edge.dst])
+      {
+        pi[edge.dst] = betterWeight;
+      }
+    }
+  }
+
+  return pi[dst];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,46 +15,40 @@ void assertRange(int value, int min, int max, string name)
   }
 }
 
-Graph processInput()
-{
-  int N, M, Q, maxM, i, u, v, w;
-
-  cin >> N >> M >> Q;
-  maxM = min(N * (N - 1), 1000);
-  assertRange(N, 2, 100, "N");
-  assertRange(M, 1, maxM, "M");
-  assertRange(Q, 1, 1000, "Q");
-
-  Graph graph = Graph(N);
-
-  for (i = 0; i < M; ++i)
-  {
-    cin >> u >> v >> w;
-    assertRange(u, 1, N, "u");
-    assertRange(v, 1, N, "v");
-    assertRange(w, 1, 100000, "w");
-    if (u == v)
-      throw invalid_argument("u and v must not be equal");
-    graph.addEdge(u, v, w);
-  }
-
-  for (i = 0; i < Q; ++i)
-  {
-    cin >> u >> v;
-    assertRange(u, 1, N, "u");
-    assertRange(v, 1, N, "v");
-    if (u == v)
-      throw invalid_argument("u and v must not be equal");
-  }
-
-  return graph;
-}
-
 int main()
 {
   try
   {
-    Graph graph = processInput();
+    int N, M, Q, maxM, i, u, v, w;
+
+    cin >> N >> M >> Q;
+    maxM = min(N * (N - 1), 1000);
+    assertRange(N, 2, 100, "N");
+    assertRange(M, 1, maxM, "M");
+    assertRange(Q, 1, 1000, "Q");
+
+    Graph graph = Graph(N);
+
+    for (i = 0; i < M; ++i)
+    {
+      cin >> u >> v >> w;
+      assertRange(u, 1, N, "u");
+      assertRange(v, 1, N, "v");
+      assertRange(w, 1, 100000, "w");
+      if (u == v)
+        throw invalid_argument("u and v must not be equal");
+      graph.addEdge(u - 1, v - 1, w);
+    }
+
+    for (i = 0; i < Q; ++i)
+    {
+      cin >> u >> v;
+      assertRange(u, 1, N, "u");
+      assertRange(v, 1, N, "v");
+      if (u == v)
+        throw invalid_argument("u and v must not be equal");
+      cout << graph.maxWeight(u - 1, v - 1) << endl;
+    }
   }
   catch (const invalid_argument &e)
   {
